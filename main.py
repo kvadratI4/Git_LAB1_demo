0393b7b2bc653ab01129d6bae12ec8064c16a2b8
rtolkachev <rtolkachev@licard.com>
2025-09-09 18:44:18 +0300

    message from evil genius

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from hello import hello
 if __name__ == "__main__":
     times = int(input("Enter times for printing: "))
     name = input("Enter name: ")
+    print('My name is Rodion from RT5')
 
     hello(times, name)
